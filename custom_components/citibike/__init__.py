1639bd11a32005c79d0e50c9b9f33387b90f1656
ruchoff <ruchman.bryce@gmail.com>
2025-03-09 03:00:34 -0400

    define CONFIG_SCHEMA

--- custom_components/citibike/__init__.py
+++ custom_components/citibike/__init__.py
@@ -1,18 +1,30 @@
+"""Integration for Citi Bike."""
+
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.typing import ConfigType
+from homeassistant.helpers import config_validation as cv
+import voluptuous as vol
 
 from .const import DOMAIN
 
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
+
+
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the Citibike integration."""
     return True
 
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Citibike from a config entry."""
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, "sensor")
     )
     return True
 
+
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
     await hass.config_entries.async_forward_entry_unload(entry, "sensor")
     return True
